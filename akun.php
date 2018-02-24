d759ae80b774f28842058144153fc79692fde323
WildanZulkarnaen <wildan.zulkarnaen@kumparan.com>
2018-02-24 08:14:40 +0700

    akun page

---
+++ akun.php
@@ -0,0 +1,34 @@
+<?php include 'components/head.php'; ?>
+
+<!-- CONTENT GOES HERE -->
+<?php include 'components/navbar-header.php'; ?>
+
+<div class="wrapper">
+  <div class="container">
+    <br />
+    <div class="card">
+        <div class="card-body pb-1">
+          <div class="text-center">
+            <img src="assets/img/sample.jpg" class="img-thumbnail img-150 rounded-circle" />
+            <h3 class="mt-2">Citra Rezania</h3>
+          </div>
+        </div>
+
+
+    </div>
+    <div class="my-3">
+      <a class="btn btn-outline-dark btn-block" href="home-user.php">Logout</a>
+    </div>
+  </div>
+  </div>
+
+
+</div>
+
+
+<?php include 'components/navbar-bottom.php'; ?>
+<?php include 'components/modal.php'; ?>
+
+<!-- END CONTENT GOES HERE -->
+
+<?php include 'components/footer.php'; ?>
